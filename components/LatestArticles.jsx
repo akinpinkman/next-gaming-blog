46cac708197880a143349420f63d5235d488f4a0
akinpinkman <akinbilgin54@hotmail.com>
2024-03-24 11:04:02 +0300

    Fixed the Upcoming Games layout
    Added styling and fix the layout of the upcoming games

--- components/LatestArticles.jsx
+++ components/LatestArticles.jsx
@@ -4,7 +4,7 @@ import ReleaseDateTracker from "./ReleaseDateTracker";
 export default function LatestArticles({ articles, games }) {
   return (
     <div className="flex ml-[315px] mt-10 space-x-[710px]">
-      <div className="flex flex-col gap-5 ">
+      <div className="flex flex-col gap-5">
         <div className="flex items-center gap-5">
           <span className="bg-blue-500 w-5 h-5"></span>
           <h1 className="text-xl font-bold">Latest Articles</h1>
