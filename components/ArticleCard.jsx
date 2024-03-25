bed2b6fe1b38e776fc9045868be6d9bb6972fa92
akinpinkman <akinbilgin54@hotmail.com>
2024-03-25 18:05:46 +0300

    Handle routing
    When user clicked the article it redirect to the articles slug

--- components/ArticleCard.jsx
+++ components/ArticleCard.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import fixMetaTag from "@/utils/fixMetaTags";
+import Link from "next/link";
 
 export default function Article({ article, isFirst }) {
   const { heading, subtitle, slug, featureImage, articleDate, author } =
@@ -11,26 +12,29 @@ export default function Article({ article, isFirst }) {
 
   return (
     <article className="mt-10 relative">
-      <div style={{ position: "relative" }}>
-        <Image
-          src={image}
-          alt={heading}
-          width={imageWidth}
-          height={imageHeight}
-          priority
-        />
-        <li
-          className="list-none bg-blue-500 px-2 rounded-sm"
-          style={{ position: "absolute", bottom: "1rem", left: "1rem" }}
-        >
-          <a href="PATH/TO/THIS/TAG" className="font-bold text-white">
-            {fixMetaTag({ article })}
-          </a>
-        </li>
-      </div>
-
-      <h1 className="text-xl">{article.fields.heading}</h1>
-      <h2 className="text-lg">{article.fields.subtitle}</h2>
+      <Link href={`/article/${slug}`} passHref>
+        <div className="mt-10 relative" style={{ textDecoration: "none" }}>
+          <div style={{ position: "relative" }}>
+            <Image
+              src={image}
+              alt={heading}
+              width={imageWidth}
+              height={imageHeight}
+              priority
+            />
+            <li
+              className="list-none bg-blue-500 px-2 rounded-sm"
+              style={{ position: "absolute", bottom: "1rem", left: "1rem" }}
+            >
+              <span className="font-bold text-white">
+                {fixMetaTag({ article })}
+              </span>
+            </li>
+          </div>
+          <h1 className="text-xl">{heading}</h1>
+          <h2 className="text-lg">{subtitle}</h2>
+        </div>
+      </Link>
     </article>
   );
 }
--- pages/article/[slug].js
+++ pages/article/[slug].js
@@ -3,13 +3,13 @@ import { useRouter } from "next/router";
 const Article = () => {
   const router = useRouter();
   const { slug, heading, subtitle } = router.query;
-  console.log(heading);
+  console.log();
 
   return (
     <>
       <p>This is the page for post: {slug}</p>
-      <h1>A{heading}</h1>
-      <h2>A{subtitle}</h2>
+      <h1>{heading}</h1>
+      <h2>{subtitle}</h2>
     </>
   );
 };
