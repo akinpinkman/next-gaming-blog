7324c9d8bb1e9e379e62e5653a791d3e0c6e51af
akinpinkman <akinbilgin54@hotmail.com>
2024-04-02 11:02:01 +0300

    Footer fixed to the bottom of the page

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,9 +3,9 @@ import Header from "./Header";
 
 export default function Layout({ children }) {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <main>{children}</main>
+      <main className="flex-grow">{children}</main>
       <Footer />
     </div>
   );
